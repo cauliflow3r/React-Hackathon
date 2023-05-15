9844cc0357a504d33362d2064fc12cc50b04f65a
Azamat <cauliflow3r@MacBook-Air-Nurik.local>
2023-05-15 23:49:42 +0600

    hz

--- src/components/navbar/AxyenniyNavbar.jsx
+++ src/components/navbar/AxyenniyNavbar.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import icon_black from "../svg/logo_white.svg";
 import "../Styles/AxyenniyNavbar.css";
 import { Box } from "@mui/material";
 import siteName from "../svg/siteName.png";
 import { Link } from "react-router-dom";
+import $ from "jquery";
 
 const AxyenniyNavbar = () => {
+  useEffect(() => {
+    // Responsive-navbar-active-animation
+    function test() {
+      var tabsNewAnim = $("#navbarSupportedContent");
+      var selectorNewAnim = $("#navbarSupportedContent").find("li").length;
+      var activeItemNewAnim = tabsNewAnim.find(".active");
+
+      if (activeItemNewAnim.length > 0) {
+        var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+        var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+        var itemPosNewAnimTop = activeItemNewAnim.position();
+        var itemPosNewAnimLeft = activeItemNewAnim.position();
+
+        $(".hori-selector").css({
+          top: itemPosNewAnimTop.top + "px",
+          left: itemPosNewAnimLeft.left + "px",
+          height: activeWidthNewAnimHeight + "px",
+          width: activeWidthNewAnimWidth + "px",
+        });
+      }
+    }
+
+    // Add active class on another page linked
+    var path = window.location.pathname.split("/").pop();
+    if (path === "") {
+      path = "index.html";
+    }
+    var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
+    target.parent().addClass("active");
+  }, []);
+
+  // Get the current date
+  const currentDate = new Date();
+  const formattedDate = currentDate.toDateString();
+
   return (
     <div>
       <Box
@@ -36,34 +72,41 @@ const AxyenniyNavbar = () => {
               alt="logo"
             />
             <img style={{ maxWidth: "150px" }} src={siteName} alt="" />
-            <div className="search">
-              <input
-                className="input"
-                type="text"
-                placeholder="Search"
-                name="text"
-              />
+          </div>
+          <div className="date">
+            <p className="current-date">{formattedDate}</p>
+          </div>
+          <div className="search">
+            <input
+              className="input"
+              type="text"
+              placeholder="Search"
+              name="text"
+            />
+            <button>
+              <span className="button_top">Search</span>
+            </button>
+          </div>
+          <div className="navigation_buttons">
+            <Link to="/draw">
               <button>
-                <span class="button_top">Search</span>
+                <span className="button_top">Draw</span>
               </button>
-            </div>
-            <div className="navigation_buttons">
-              <Link to="/draw">
-                <button>
-                  <span class="button_top"> Draw</span>
-                </button>
-              </Link>
-              <Link to="/products">
-                <button>
-                  <span class="button_top"> Gallery</span>
-                </button>
-              </Link>
-              <Link to="cart">
-                <button>
-                  <span class="button_top"> Cart</span>
-                </button>
-              </Link>
-            </div>
+            </Link>
+            <Link to="/products">
+              <button>
+                <span className="button_top">Gallery</span>
+              </button>
+            </Link>
+            <Link to="/cart">
+              <button>
+                <span className="button_top">Cart</span>
+              </button>
+            </Link>
+          </div>
+          <div className="hori-selector">
+            <div className="left"></div>
+            <div className="right"></div>
           </div>
         </navbar>
       </Box>
