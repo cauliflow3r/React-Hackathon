99694b6271297736821d9e089f1f5b804321dcc2
cauliflow3r <123936148+cauliflow3r@users.noreply.github.com>
2023-05-15 22:43:22 +0600

    navbar huyna

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import MainRoute from "./routes/MainRoute";
 import Navbar from "./components/navbar/Navbar";
+import AxyenniyNavbar from "./components/navbar/AxyenniyNavbar";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <AxyenniyNavbar />
       <MainRoute />
     </div>
   );
